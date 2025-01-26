c665eba1344bd94c77c115d64b3a8ddc7246925a
EMMMABK <adilbikiev2@gmail.com>
2025-01-26 13:31:04 +0600

    add Upload Admin

--- FileUploadSystem/FileUploadSystemApp/forms.py
+++ FileUploadSystem/FileUploadSystemApp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class MyFileUploadForm(forms.Form):
     file_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    files = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-control'}))
+    files = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-control '}))
