bfb1e6f333d7af5df2715e9226e407589e2b5800
EMMMABK <adilbikiev2@gmail.com>
2025-01-26 13:40:35 +0600

    add Uploaded file to DB

--- FileUploadSystem/FileUploadSystemApp/models.py
+++ FileUploadSystem/FileUploadSystemApp/models.py
@@ -6,6 +6,6 @@ class file_upload(models.Model):
     id = models.AutoField(primary_key=True)
     file_name = models.CharField(max_length=255)
     my_file = models.FileField(upload_to='')
-
+    
     def __str__(self):
         return self.file_name
--- FileUploadSystem/FileUploadSystemApp/views.py
+++ FileUploadSystem/FileUploadSystemApp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from .forms import MyFileUploadForm
+from .models import file_upload
 # Create your views here.
 
 def index(request):
-    context = {
-        'form': MyFileUploadForm()
-    }
-    return render(request, 'FileUploadSystemApp/index.html', context)
+    if request.method == "POST":
+        c_form = MyFileUploadForm(request.POST, request.FILES)
+
+        if c_form.is_valid():
+            name = c_form.cleaned_data['file_name']
+            files = c_form.cleaned_data['files']
+            file_upload(file_name=name, my_file = files).save()
+
+            return HttpResponse('File successfully uploaded!')
+        else:
+            return HttpResponse("File didn't uploaded.")
+    else:
+        context = {
+            'form': MyFileUploadForm()
+        }
+        return render(request, 'FileUploadSystemApp/index.html', context)
